cbc161dc4a26a32bbc7397d173273f71f87d3685
Matheus Felix <matheus.felix.silva@usp.br>
2018-11-11 05:19:35 -0200

    get company proposals by tag

--- app/controllers/department/overview_controller.rb
+++ app/controllers/department/overview_controller.rb
@@ -1,4 +1,8 @@
 class Department::OverviewController < Department::BaseController
   def index
+    @company_proposals = current_department.tags.map do |tag|
+      CompanyProposal.joins(:tags).where("tags.value = '#{tag.value}'").first 
+    end
+    @company_proposals = @company_proposals.uniq
   end
 end
