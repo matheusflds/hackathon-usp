82d445152eda1757a91c5a9f71c48d28eb754de7
Bruno Nishimoto <bruno_nishimoto@hotmail.com>
2018-11-11 05:52:39 -0200

    Create company proposal

--- app/controllers/company/base_controller.rb
+++ app/controllers/company/base_controller.rb
@@ -1,3 +1,7 @@
 class Company::BaseController < ApplicationController
   layout 'company'
+
+  def current_company
+    Company.first || Company.create(name: 'RedBull', email: 'red@bull.com', phone: '190')
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  namespace 'company' do 
+  namespace 'company' do
     root to: 'overview#index'
 
     resources :overview, only: %i[index]
     resources :projects
-  end 
+    resources :proposals
+  end
 
   namespace 'department' do
     root to: 'overview#index'
